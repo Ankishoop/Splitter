5e14beaf873641c0e9dc4e80254c7bb23633d0cf
Ankishoop <ankishkhandelwal705@gmail.com>
2024-09-28 03:20:53 +0530

    check 2

--- Server/index.js
+++ Server/index.js
@@ -2,4 +2,4 @@ console.log("hello world");
 console.log("hello world2");
 console.log("HELLO WORLD3");
 
-console.log("Ankish Khandelwal local to remote dev");
+console.log("update 2 time Ankish_dev to dev");
